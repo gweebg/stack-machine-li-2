dad494c9e6a3cc22d878cae5ef3667bbbafe4450
Guilherme <guilherme.g.sampaio@gmail.com>
2021-03-20 16:05:11 +0000

    Headers e modulo parser

--- code/main.c
+++ code/main.c
@@ -3,33 +3,7 @@
 # include <assert.h>
 # include <string.h>
 
-void parser(char *line) {
-   
-   char* delim = " \t\n";
-   char* token = strtok(line,delim); 
-
-   char* endptr;
-   long value;
-
-   while (token != NULL) {
-    
-    value = strtol(token, &endptr, 10);
-
-    if (strlen(endptr) == 0) {
-
-        printf("%ld --> INT\n",value);
-
-    }
-    else{
-
-        printf("%s\n", token);
-
-    }
-
-    token = strtok(NULL,delim);
-
-   }
-}
+# include "parser.h"
 
 int main() {
 
