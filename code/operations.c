2c2bf01562e3bc5451e3775cac19f01d174c738d
Guilherme <guilherme.g.sampaio@gmail.com>
2021-04-03 22:51:59 +0100

    fix nas operações

--- code/operations.c
+++ code/operations.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include "stack.h"
 #include "operations.h"
 
+// void add(stack *s)
+// {
+//     int x = pop(s).data.int_value;
+//     int y = pop(s).data.int_value;
+
+//     push(s, STACK_INT, x + y);
+// }
+
 void add(stack *s)
 {
-    int x = pop(s).data.int_value;
-    int y = pop(s).data.int_value;
+    stack_elem x = pop(s);
+    stack_elem y = pop(s);
 
-    push(s, STACK_INT, x + y);
+    stack_type x_type = x.type;
+    stack_type y_type = y.type;
+
+    if (x_type == STACK_INT && y_type == STACK_INT) push(s, STACK_INT, x.data.int_value + y.data.int_value);
+    else if (x_type == STACK_FLOAT && y_type == STACK_FLOAT) push(s, STACK_FLOAT, x.data.float_value + y.data.float_value);
+    else if (x_type == STACK_DOUBLE && y_type == STACK_DOUBLE) push(s, STACK_DOUBLE, x.data.double_value + y.data.double_value);
+    else if (x_type == STACK_INT && y_type == STACK_FLOAT) push(s, STACK_FLOAT, (x.data.int_value) + y.data.float_value);
+    else if (x_type == STACK_FLOAT && y_type == STACK_INT) push(s, STACK_FLOAT, (x.data.float_value + y.data.int_value));
+    else { fprintf(stderr, "Operação inválida!\n"); exit(EXIT_FAILURE); }
+            
 }
 
 void sub(stack *s)
